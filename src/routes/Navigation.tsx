8f1f78b0c7485f3e02a56d4f418e177282a0c641
Sebastian Parra <juan.jparra@mercadolibre.com.co>
2022-12-27 23:21:56 -0500

    lazyload Basico

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -9,46 +9,26 @@ import logo from '../logo.svg'
 
 export const Navigation = () => {
     return (
-        <Suspense fallback={ <span>Loading...</span> }>
-
+        <Suspense fallback={<span>Loading...</span>}>
             <BrowserRouter>
                 <div className="main-layout">
                     <nav>
                         <img src={ logo } alt="React Logo" />
                         <ul>
-                            
-                            {
-                                routes.map(({ to, name }) => (
-                                    <li key={ to }>
-                                        <NavLink 
-                                            to={ to } 
-                                            className={ ({ isActive }) => isActive ? 'nav-active' : '' }>
-                                            { name }
-                                        </NavLink>
-                                    </li>
-                                ))
-                                    
-                            }
-                            
-                            
+                            {routes.map(({to, name}) => (
+                                <li key={to}>
+                                    <NavLink to={to} className={ ({ isActive }) => isActive ? 'nav-active' : '' }>{name}</NavLink>
+                                </li>
+                            ))}
                         </ul>
                     </nav>
 
 
                     <Routes>
-                        
-                        {
-                            routes.map( ({ path, Component  }) => (
-                                <Route 
-                                    key={ path }
-                                    path={ path }
-                                    element={ <Component /> } 
-                                />
-                            ))
-                        }
-
-                        
-                        <Route path="/*" element={ <Navigate to={ routes[0].to } replace /> } />
+                        {routes.map(({path, Component}) => (
+                            <Route key={path} path={path} element={ <Component /> } />
+                        ))}
+                        <Route path="/*" element={ <Navigate to={routes[0].to} replace /> } />
                     </Routes>
 
                 </div>
